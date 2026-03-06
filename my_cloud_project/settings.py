7bc78f9169e64a3c7e42709b0c2bb5a114bd11b2
Smorodin_SA <ssmorodin@bk.ru>
2026-03-07 01:14:27 +0300

    админка, переключение прав

--- my_cloud_project/settings.py
+++ my_cloud_project/settings.py
@@ -108,6 +108,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
